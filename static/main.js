649d3d60ea411db967b5618d2c6af3542830867c
Matt Del Signore <mattdelsig@me.com>
2016-02-22 15:06:04 -0500

    Added support for polling for the photos

--- static/main.js
+++ static/main.js
@@ -3,16 +3,33 @@ $(function(){
   console.log("document loaded!");
 
   var slideImage = $("#slide"); //get the image
+  var greeting = $("#greeting");
+  var date = new Date();
 
-  //make the ajax request to the server to get the image urls
-  //TODO: make it poll for data
-  $.ajax({
-    url:"/image",
-    type:"GET",
-    dataType:"text",
-    success: function(text){
-      console.log(text);
-      slideImage.attr("src",text);
-    }
-  });
+  //change the greeting based on the time of day
+  var time = date.getHours();
+
+  if(time >0 && time <=12){
+    greeting.text("Good Morning ");
+  }else if(time >12 && time < 17){
+    greeting.text("Good Afternoon ");
+  }else{
+    greeting.text("Good Evening ");
+  }
+
+//make the ajax request to the server to get the image urls
+(function poll(){
+  //poll for data with a 6 second timout
+    $.ajax({
+        url:"/image",
+        type:"GET",
+        dataType:"text",
+        success: function(text){
+          console.log(text);
+          slideImage.attr("src",text);
+        },
+        complete: setTimeout(poll, 6000),
+        timeout: 2000
+      });
+})();
 });
