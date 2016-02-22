e1e5685ee46dc0ae69838246f05c0ad86fc997cd
Matt Del Signore <mattdelsig@me.com>
2016-02-22 13:17:27 -0500

    Adds a random image to the pages from the server

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask,session,request,render_template,url_for
-
+import os
+from os import path
+import random
 #initialize our flask app object
 app = Flask(__name__)
-name = "Nancy"
+name = "Nancy" #name of the user
+imagepaths = [] #image paths
+base_img_path = "./static/images/"
+
 #set the root directort of the site to render the template for the page
 @app.route('/')
 def homepage():
@@ -11,6 +16,19 @@ def homepage():
 
 #TODO: add file upload ability
 
+#the route for getting a random image from the images folder
+@app.route('/image')
+def random_image():
+    print base_img_path+random.choice(imagepaths)
+    #return a random image url
+    return base_img_path + random.choice(imagepaths)
+
 #code to run the app
 if __name__ == '__main__':
+    #get all the image paths before the app runs
+    #check if it's a file and then add is to the image paths if it is using a python list comprehension
+    imagepaths = [img for img in os.listdir(base_img_path) if img.endswith(".jpeg") or img.endswith(".jpg") or img.endswith(".png")]
+    print "imagepaths:" + str(imagepaths)
+    #set up debug mode so the server will automatically reload
+    app.debug = True
     app.run()#run with the debug flag on
---
+++ static/main.js
@@ -0,0 +1,17 @@
+//use the shorthand for document ready
+$(function(){
+  console.log("document loaded!");
+
+  var slideImage = $("#slide"); //get the image
+
+  //make the ajax request to the server to get the image urls
+  $.ajax({
+    url:"/image",
+    type:"GET",
+    dataType:"text",
+    success: function(text){
+      console.log(text);
+      slideImage.attr("src",text);
+    }
+  });
+});
