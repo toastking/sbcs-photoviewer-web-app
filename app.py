a62338b13aeb68f0ed7b122bf3653cb3650f8b6e
Matt Del Signore <mattdelsig@me.com>
2016-03-12 16:25:39 -0500

    changed polling interval and added support for updating the image paths

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ from flask import Flask,request,render_template,url_for,send_from_directory
 import os
 import random
 import itertools
-import logging 
-import sys 
+import logging
+import sys
 
 #initialize our flask app object
 app = Flask(__name__)
 name = "Nancy" #name of the user
 
-basepath = os.path.dirname(__file__) 
-imagepaths = ["pic1.jpeg", "pic2.jpg","pic3.jpg"]
-print "imagepaths:" + str(imagepaths)
-iterator = itertools.cycle(imagepaths)
+basepath = os.path.dirname(__file__)
+imagepaths = []
+
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
@@ -36,23 +35,25 @@ def homepage():
 def random_image():
     #return a random image url
     url =  url_for('static', filename = 'images/'+iterator.next())
-        
-    print "url: " + url
     return url
 
+#the route for getting a random image from the images folder
+@app.route('/update')
+def update_image():
+    #check if it's a file and then add is to the image paths if it is using a python list comprehension
+    imagepaths = [img for img in os.listdir(basepath +'/static/' + base_img_path) if img.endswith(".jpeg") or img.endswith(".jpg") or img.endswith(".png") or img.endswith(".PNG")or img.endswith(".gif")]
+    #set up debug mode so the server will automatically reload
+    print "imagepaths:" + str(imagepaths)
+    iterator = itertools.cycle(imagepaths)
+    return "array update sucessfully"
 
-#@app.route('/images/<path:filename>')
-#def images(filename):
-    #return a random image url
- #   url =  url_for('images')
- #   print "imgurl: " + url
- #   return send_from_directory(url,filename)
-    
 #code to run the app
 if __name__ == '__main__':
     #get all the image paths before the app runs
     #check if it's a file and then add is to the image paths if it is using a python list comprehension
-    #imagepaths = [img for img in os.listdir(basepath +'/static/' + base_img_path) if img.endswith(".jpeg") or img.endswith(".jpg") or img.endswith(".png")
+    imagepaths = [img for img in os.listdir(basepath +'/static/' + base_img_path) if img.endswith(".jpeg") or img.endswith(".jpg") or img.endswith(".png") or img.endswith(".PNG")or img.endswith(".gif")]
     #set up debug mode so the server will automatically reload
-    #app.debug = True
+    print "imagepaths:" + str(imagepaths)
+    iterator = itertools.cycle(imagepaths)
+    app.debug = True
     app.run()#run with the debug flag on
--- static/main.js
+++ static/main.js
@@ -2,12 +2,22 @@
 $(function(){
   console.log("document loaded!");
 
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+  var months  = ["January", "February", "March", "April", "May","June","July","August","September","October","November","December"];
+
   var slideImage = $("#slide"); //get the image
   var greeting = $("#greeting");
+  var dateText = $("#date");
   var date = new Date();
 
   //change the greeting based on the time of day
   var time = date.getHours();
+  var dayOfWeek = days[date.getDay()];
+  var month = months[date.getMonth()];
+  var dayNumber = date.getDate();
+  var year = date.getFullYear();
+
+  dateText.text(dayOfWeek + " " + month + " " + dayNumber + ", " + year);
 
   if(time >0 && time <=12){
     greeting.text("Good Morning ");
@@ -31,8 +41,27 @@ $(function(){
             console.log(error);
         }
         ,
-        complete: setTimeout(poll, 6000),
+        complete: setTimeout(poll, 10000),
         timeout: 2000
       });
 })();
+//make the ajax request to the server to get the image urls
+var pollingInterval = 1000 * 60 * 10;
+(function run(){
+  //poll for data with a 6 second timout
+    $.ajax({
+        url:"/update",
+        type:"GET",
+        dataType:"text",
+        success: function(data){
+          console.log(data);
+        },error: function(error) {
+            console.log(error);
+        }
+        ,
+        complete: setTimeout(run, pollingInterval),
+        timeout: 2000
+      });
+})();
+
 });
